ae27636ac8aa6baa7130df1c0516f9b1fb2f8208
cjns <67065839+cjns@users.noreply.github.com>
2023-07-19 11:18:01 +0100

    Complete reduce() examples

--- reduce.js
+++ reduce.js
@@ -6,7 +6,17 @@
 
 
 // Summing an array of numbers:
-
+const nums = [0, 1, 2, 3, 4];
+// let sum = nums.reduce((acc, curr) => {
+//   console.log(
+//     "Accumulator:", acc,
+//     "Current Value:", curr,
+//     "Total", acc + curr
+//   );
+//   return acc + curr;
+// }, 10);
+let sum = nums.reduce((acc, curr) => acc + curr, 0);
+console.log(sum);
 
 const teamMembers = [
   {
@@ -28,10 +38,45 @@ const teamMembers = [
     name: 'Kelly',
     profession: 'Designer',
     yrsExperience: 3
+  },
+  {
+    name: 'Mark',
+    profession: 'Manager',
+    yrsExperience: 10
   }
 ];
 
 // Totaling a specific object property
-
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
 
 // Grouping by a property, and totaling it too
+// {Developer: 12. Designer 4} <-- This is what we want.
+let experienceByProfession  = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+
+console.log(experienceByProfession);
+
+// Create an object that has the professions as property names with the names of the employees, in an array, as a value.
+// E.G. { Developer: ['Andrew', 'Ariel'], Designer: ['Michael', 'Kelly'], Manager: ['Mark']}
+let namesByProfession = teamMembers.reduce((acc, curr) => {
+  let profession = curr.profession;
+  if (!acc[profession]) {
+    acc[profession] = [curr.name];
+  } else {
+    acc[profession].push(curr.name);
+  }
+  return acc;
+}, {});
+
+console.log(namesByProfession);
+
+// Consider 100 different employees and 20 different professions. How would you filter out those in a specific profession and find the one with the most experience?
+// This could be done with different combinations of map(), filter(), and reduce().
